cfe0bddacea64d8a03bce20cc9eb303f0efc42ff
GOTO Kenta <gotoukenta62@gmail.com>
2024-06-28 10:53:06 +0900

    Hot: initial revision

---
+++ HeadsorTails.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <random>
+
+using namespace std;
+
+int main()
+{
+    random_device seed_generator; //hardware random value generator
+    mt19937 engine(seed_generator()); //Using Mersenne twister 
+    bernoulli_distribution distribution(0.5); //Make a bernoulli_distribution -> random value = 0.5
+    int headsCounter = 0; //counter for heads
+    int tailsCounter = 0; //counter for tails
+
+
+    cout << "Tossing a coin\n";
+
+    for(int i = 0 ; i < 3 ; i++)
+    {
+
+        cout << "Round " << i + 1 << ": ";
+
+        switch(distribution(engine))
+        {
+            case true: 
+                cout << "Heads";
+                headsCounter++;
+                break;
+            case false:
+                cout << "Tails";
+                tailsCounter++;
+                break;
+        }
+
+        cout << "\n";
+    }
+
+    cout << "Heads: " << headsCounter << ", Tails: " << tailsCounter << "\n";
+}
