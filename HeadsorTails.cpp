58460a241caf3fae3d37002702af96e13fb526f5
GOTO Kenta <gotoukenta62@gmail.com>
2024-06-28 11:00:55 +0900

    User name added

--- HeadsorTails.cpp
+++ HeadsorTails.cpp
@@ -1,38 +1,14 @@
 #include <iostream>
-#include <random>
 
 using namespace std;
 
 int main()
 {
-    random_device seed_generator; //hardware random value generator
-    mt19937 engine(seed_generator()); //Using Mersenne twister 
-    bernoulli_distribution distribution(0.5); //Make a bernoulli_distribution -> random value = 0.5
-    int headsCounter = 0; //counter for heads
-    int tailsCounter = 0; //counter for tails
+    string str;
 
+    cout << "Who are you?";
 
-    cout << "Tossing a coin\n";
+    cin >> str;
 
-    for(int i = 0 ; i < 3 ; i++)
-    {
-
-        cout << "Round " << i + 1 << ": ";
-
-        switch(distribution(engine))
-        {
-            case true: 
-                cout << "Heads";
-                headsCounter++;
-                break;
-            case false:
-                cout << "Tails";
-                tailsCounter++;
-                break;
-        }
-
-        cout << "\n";
-    }
-
-    cout << "Heads: " << headsCounter << ", Tails: " << tailsCounter << "\n";
+    cout << "Hello, John!";
 }
